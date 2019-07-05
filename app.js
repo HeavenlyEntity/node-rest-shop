31230485ab2ea9c675816796ac0c2213f6168e2d
Alec M <39228141+HeavenlyEntity@users.noreply.github.com>
2019-07-05 16:27:07 -0700

    Added Header Authenication

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ const productRoutes = require('./API/routes/products');
 const orderRoutes = require('./API/routes/orders');
 
 app.use(morganLogger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
+
 
 
 //Routes being used for API URL Requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === "OPTIONS") {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404;
@@ -28,4 +41,6 @@ app.use((error, req, res, next) => {
         }
     })
 });
+
+
 module.exports = app;
