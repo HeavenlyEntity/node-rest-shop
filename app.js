e93b7eac3606fcee6f7197e76d3dd2a99cbc8231
Alec M <39228141+HeavenlyEntity@users.noreply.github.com>
2019-07-05 00:48:23 -0700

    Updated Packages - updating error handling

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const morganLogger = require('morgan');
 
 const productRoutes = require('./API/routes/products');
 const orderRoutes = require('./API/routes/orders');
 
+app.use(morganLogger('dev'));
+
+//Routes being used for API URL Requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req,res,next) => {
+
+});
+
+app.use((req,res,next) => {
+
+});
 module.exports = app;
